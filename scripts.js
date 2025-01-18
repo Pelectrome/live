74e7b09803c992bf6112ae9c9dcce7870868b0c6
pelectrome <pelectrome@gmail.com>
2025-01-18 09:28:37 -0500

    modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -179,6 +179,7 @@ function openPopup(current_ticket, office_number) {
        // Clear the previous timeout if it exists
        if (timeoutId) {
         clearTimeout(timeoutId);
+        console.log("Popup closed");
     }
 
     timeoutId = setTimeout(closePopup, 5000);
